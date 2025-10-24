6b3e0f30587b19904c5b64d342fc7dd5384f6ee2
Vu Duc Dung <dungvd0309@gmail.com>
2025-10-24 16:11:30 +0700

    chore: trying PID with encoders' positions

--- esp32-firmware/esp32-firmware.ino
+++ esp32-firmware/esp32-firmware.ino
@@ -15,8 +15,8 @@
 
 #define S1L 16
 #define S2L 17
-#define S1R 18
-#define S2R 5
+#define S1R 5
+#define S2R 18
 // ===== END ===== //
 
 // ===== KHAI BÁO HẰNG SỐ ===== //
@@ -24,6 +24,7 @@ const int freq = 20000;    // tần số PWM 20 kHz
 const int channel = 0;     // kênh PWM (0-15)
 const int resolution = 8;  // độ phân giải bit (8 bit cho giá trị từ 0-255)
 
+const int MIN_PWM = 135;             // Giá trị PWM tối thiểu để động cơ bắt đầu quay
 // ref: JGA25-370 280RPM 
 const double GEAR_RATIO = 20.4 / 1; // hệ số bánh răng của động cơ
 const int DECODE_FACTOR = 1;        // hệ số giải mã xung A/B (x1, x2, x4), trong file motor_encoder đang là x1 (đếm RISING của S1)
@@ -36,7 +37,7 @@ MotorController controller(ENA, IN1, IN2, IN3, IN4, ENB);
 MotorEncoders encoders(S1L, S2L, S1R, S2R, WHEEL_CPR, WHEEL_DIAMETER / 2); 
 
 double Setpoint = 0, Input = 0, Output = 0;
-double Kp = 2.0, Ki = 5.0, Kd = 1.0;
+double Kp = 1.2, Ki = 0.0, Kd = 0.1;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 String inputString = "";    // Biến lưu chuỗi serial nhận
@@ -50,10 +51,9 @@ void setup()
   controller.movePWM(0,0);
 
   // Đặt tần số PWM cao để giảm tiếng động cơ
-  // ledcDetachPin(ENA);
-  // ledcSetup(channel, freq, resolution);  // thiết lập kênh PWM với tần số và độ phân giải
-  // ledcAttachPin(ENA, channel);      // gán chân PWM cho kênh
-  // ledcAttachChannel(ENA, freq, resolution, channel);
+  ledcDetachPin(ENA);
+  ledcSetup(channel, freq, resolution);  // thiết lập kênh PWM với tần số và độ phân giải
+  ledcAttachPin(ENA, channel);      // gán chân PWM cho kênh
 
   // FreeRTOS
   // xTaskCreatePinnedToCore(ros_task, "ros", 8192, NULL, 2, NULL, 0);
@@ -61,8 +61,8 @@ void setup()
   // Serial.println("Angle(rad),Setpoint(rad),Output(PWM)");
   // inputString.reserve(50);
 
-  // myPID.SetMode(AUTOMATIC);
-  // myPID.SetOutputLimits(-255, 255);
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(-255, 255);
 }
 
 void ros_task(void*) {
@@ -72,47 +72,51 @@ void ros_task(void*) {
 
 void loop()
 {
-  // // Đọc encoder
-  // Input = encoders.getLeftAngle();
+  // Đọc encoder
+  Input = encoders.getLeftAngle();
+  Setpoint = encoders.getRightAngle();
 
-  // // Tính PID
-  // myPID.Compute();
+  // Tính PID
+  myPID.Compute();
 
-  // // Gửi tín hiệu PWM
-  // // controller.movePWM((int)Output,(int)Output);
-  // digitalWrite(IN1, Output < 0 ? HIGH : LOW);
-  // digitalWrite(IN2, Output < 0 ? LOW : HIGH);
-  // ledcWrite(channel, abs((int)Output));
+  // Ánh xạ lại giá trị Output để loại bỏ vùng chết của động cơ
+  int motorPWM = (Output == 0) ? 0 : map(abs(Output), 0, 255, MIN_PWM, 255);
 
-  // // Gửi dữ liệu cho Serial Plotter
-  // Serial.print(Input, 2); Serial.print(",");
-  // Serial.print(Setpoint, 2); Serial.print(",");
-  // Serial.print(Output, 2); Serial.print(",");
-  // Serial.println();
-  // // Kiểm tra có dữ liệu Serial mới nhập
-  // if (stringComplete) {
-  //   parseInput(inputString);
-  //   inputString = "";
-  //   stringComplete = false;
-  // }
+  // Gửi tín hiệu PWM
+  // controller.movePWM((int)Output,(int)Output);
+  digitalWrite(IN1, Output < 0 ? HIGH : LOW);
+  digitalWrite(IN2, Output < 0 ? LOW : HIGH);
+  ledcWrite(channel, motorPWM);
+
+  // Gửi dữ liệu cho Serial Plotter
+  Serial.print(Input, 2); Serial.print(",");
+  Serial.print(Setpoint, 2); Serial.print(",");
+  Serial.print(Output, 2); Serial.print(",");
+  Serial.println();
+  // Kiểm tra có dữ liệu Serial mới nhập
+  if (stringComplete) {
+    parseInput(inputString);
+    inputString = "";
+    stringComplete = false;
+  }
   delay(20);
 
   // vTaskDelay(pdMS_TO_TICKS(1000)); 
 
   // FOR TESTING
 
-  encoders.updateVelocities();
-  Serial.print("count: ");
-  Serial.print(encoders.getLeftCount());
-  Serial.print("\t pos: ");
-  Serial.print(encoders.getLeftAngle());
-  Serial.print("\t rad/s: ");
-  Serial.print(encoders.getLeftAngularVelocity());
-  Serial.print("\t rpm: ");
-  Serial.print(encoders.getLeftRpm());
-  Serial.print("\t m/s: ");
-  Serial.print(encoders.getLeftLinearVelocity());
-  Serial.println();
+  // encoders.updateVelocities();
+  // Serial.print("count: ");
+  // Serial.print(encoders.getLeftCount());
+  // Serial.print("\t pos: ");
+  // Serial.print(encoders.getLeftAngle());
+  // Serial.print("\t rad/s: ");
+  // Serial.print(encoders.getLeftAngularVelocity());
+  // Serial.print("\t rpm: ");
+  // Serial.print(encoders.getLeftRpm());
+  // Serial.print("\t m/s: ");
+  // Serial.print(encoders.getLeftLinearVelocity());
+  // Serial.println();
 }
 
 void serialEvent() {
@@ -136,7 +140,7 @@ void parseInput(String s) {
   int n = sscanf(s.c_str(), "%f %f %f %f", &sp, &p, &i, &d);
   
   if (n == 4) {
-    Setpoint = sp;
+    // Setpoint = sp;
     Kp = p;
     Ki = i;
     Kd = d;
